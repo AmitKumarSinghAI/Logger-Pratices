928a227b5cfd5e33ba2cece9d9c19ba1996b5d88
AmitKumarSinghAI <amitkumarsinghkurmi123@gmail.com>
2026-08-20 15:02:26 +0530

    adding realistic_configuration.py file

--- Realistic_Configuration.py
+++ Realistic_Configuration.py
@@ -1,1 +1,37 @@
 import logging
+
+logger = logging.getLogger(__name__)
+
+logger.setLevel(logging.DEBUG)
+
+
+formatter = logging.Formatter(
+    "%(asctime)s | %(name)s | %(levelname)s | %(message)s"
+)
+
+# File Handler
+file_handler = logging.FileHandler("app.log")
+file_handler.setLevel(logging.DEBUG)
+
+file_handler.setFormatter(formatter)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+logger.addHandler(console_handler)
+
+
+logger.debug("Debug message")
+
+logger.info("Application started")
+
+logger.warning("Low memory")
+
+logger.error("Database error")
+
+logger.critical("Application crashed")
